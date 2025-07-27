69c3e2c90ff2b11d2d5d2d626aac0b981c3373c8
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-07-27 07:15:15 +0300

    added hero section contnent

---
+++ src/components/HeroSec/ContentSec.js
@@ -0,0 +1,14 @@
+
+
+function ContentSec() {
+    return(
+        <div class="content">
+            <div class="container">
+                <h1>Material Kit 2 React</h1>
+                <p>Free & Open Source Web UI Kit built over ReactJS & MUI. Join over 1.6 million developers around the world.</p>
+            </div>
+        </div>
+    )
+}
+
+export default ContentSec;
--- src/components/HeroSec/HeroSec.js
+++ src/components/HeroSec/HeroSec.js
@@ -1,11 +1,13 @@
 import HeaderSec from './HeaderSec';
 import background from '../../images/bg-presentation.jpg';
+import ContentSec from './ContentSec';
 
 
 function HeroSec() {
     return(
         <div className="hero">
             <HeaderSec />
+            <ContentSec />
         </div>
     )
 }
