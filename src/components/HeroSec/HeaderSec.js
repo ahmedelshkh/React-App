cba8ac076c37aa791f2eed4c393f44943040eb1d
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-07-27 06:35:43 +0300

    added the header

---
+++ src/components/HeroSec/HeaderSec.js
@@ -0,0 +1,78 @@
+
+
+function HeaderSec() {
+    return(
+        <header>
+            <a href="/" class="logo">Material Kit 2</a>
+            <div>
+                <ul class="nav">
+                    <li>
+                        <a href="#">
+                            <svg fill="#344767" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M112,44H48a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,112,44Zm-4,64H52V52h56ZM208,44H144a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,208,44Zm-4,64H148V52h56Zm-92,32H48a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,112,140Zm-4,64H52V148h56Zm100-64H144a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,208,140Zm-4,64H148V148h56Z">
+                                    </path>
+                                </g>
+                            </svg>
+                            <span>
+                                Pages
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg fill="#344767" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M112,44H48a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,112,44Zm-4,64H52V52h56ZM208,44H144a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,208,44Zm-4,64H148V52h56Zm-92,32H48a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,112,140Zm-4,64H52V148h56Zm100-64H144a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,208,140Zm-4,64H148V148h56Z">
+                                    </path>
+                                </g>
+                            </svg>
+                            <span>
+                                Sections
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg fill="#344767" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M112,44H48a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,112,44Zm-4,64H52V52h56ZM208,44H144a4,4,0,0,0-4,4v64a4,4,0,0,0,4,4h64a4,4,0,0,0,4-4V48A4,4,0,0,0,208,44Zm-4,64H148V52h56Zm-92,32H48a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,112,140Zm-4,64H52V148h56Zm100-64H144a4,4,0,0,0-4,4v64a4.0002,4.0002,0,0,0,4,4h64a4.0002,4.0002,0,0,0,4-4V144A4,4,0,0,0,208,140Zm-4,64H148V148h56Z">
+                                    </path>
+                                </g>
+                            </svg>
+                            <span>
+                                Docs
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit css-1amtie4" focusable="false" viewBox="0 0 24 24"
+                                aria-hidden="true" data-testid="GitHubIcon">
+                                <path
+                                    d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27">
+                                </path>
+                            </svg>
+                            <span>
+                                Github
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <a class="btn" href="#">Free Download</a>
+            </div>
+        </header>
+    )
+}
+
+
+export default HeaderSec;
---
+++ src/components/HeroSec/HeroSec.js
@@ -0,0 +1,13 @@
+import HeaderSec from './HeaderSec';
+import background from '../../images/bg-presentation.jpg';
+
+
+function HeroSec() {
+    return(
+        <div className="hero">
+            <HeaderSec />
+        </div>
+    )
+}
+
+export default HeroSec;
